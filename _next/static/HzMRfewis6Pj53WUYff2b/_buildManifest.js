970e090ec28ff764be41bc1839726ab4a0bb8f76
kowoohyuk <kowoohyuk91@gmail.com>
2023-12-21 16:54:18 +0900

    Updates

--- _next/static/HzMRfewis6Pj53WUYff2b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-35bef7184ba5c1de.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
